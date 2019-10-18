7b85a10c28d40753f200085ea8e0ed9f47753472
Nick <nick.gilliam2@gmail.com>
2019-10-18 17:29:05 -0400

    Added other pages' default structures

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('about/', views.about, name = 'about'),
+    path('work/', views.work, name = 'work'),
+    path('services/', views.services, name = 'services'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def work(request):
+    return render(request, 'main/work.html')
+
+def services(request):
+    return render(request, 'main/services.html')
