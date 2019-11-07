a7110c1764834468658e6da8662561bc53de15a4
Nick <nick.gilliam2@gmail.com>
2019-11-07 17:02:16 -0500

    Expanded support site

--- support/views.py
+++ support/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('')
+def index(request, option):
+    return render_to_response('support/index.html', {'option': option + '.html'})
